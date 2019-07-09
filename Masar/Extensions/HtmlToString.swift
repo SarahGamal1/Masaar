456b51ce98edd823c9aec0b1676930215b211915
Sarah Gamal <Sarahgamal864@gmail.com>
2019-07-10 01:56:50 +0200

    Initial commit

---
+++ Masar/Extensions/HtmlToString.swift
@@ -0,0 +1,32 @@
+//
+//  HtmlToString.swift
+//  Masar
+//
+//  Created by Sarah Gamal on 7/9/19.
+//  Copyright © 2019 Sarah Gamal. All rights reserved.
+//
+
+import UIKit
+
+
+extension String {
+    
+    var htmlToAttributedString: NSAttributedString? {
+        guard
+            let data = self.data(using: .utf8)
+            else { return nil }
+        do {
+            return try NSAttributedString(data: data, options: [
+                NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.html,
+                NSAttributedString.DocumentReadingOptionKey.characterEncoding: String.Encoding.utf8.rawValue
+                ], documentAttributes: nil)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+            return  nil
+        }
+    }
+    
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
+}
